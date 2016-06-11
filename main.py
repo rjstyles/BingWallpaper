c7e73ddef05e40a01750388c118bf132c376fbe3
Rajeev Singh <rajeevsinxh786@gmail.com>
2016-06-12 04:40:09 +0530

    updated download url for image
    To download better quality image !

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ URL = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
 try:
     image_data = json.loads(requests.get(URL).text)
     image_url = 'http://www.bing.com' + image_data['images'][0]['url']
+    
+    # updated url to download quality image
+    image_download_url = 'http://www.bing.com/hpwp/' + image_data['images'][0]['hsh']
     image_name = image_url[re.search("rb/", image_url).end():re.search('_EN', image_url).start()] + '.jpg'
     
-    # add your own path to download the image
+    # add your own path to download image
     file_path = ''+image_name
     if os.path.exists(file_path) is False:
-        urllib.request.urlretrieve(image_url, filename=file_path)
+        urllib.request.urlretrieve(image_download_url, filename=file_path)
         image_desc = image_data['images'][0]['copyright']
-        # modify below line as per your file path
+
+    # update the path below as per location of your image
         command = 'gsettings set org.gnome.desktop.background picture-uri file://'+file_path
         os.system(command)
         notify = 'notify-send -u critical "Wallpaper for the Day updated!" "' + image_desc + '"'
@@ -26,5 +30,5 @@ try:
         notify = 'notify-send -u critical "Bing Wallpaper" "Wallpaper for the day has been updated!"'
         os.system(notify)
 except:
-    notify = 'notify-send -u critical "Bing Wallpaper" "Wallpaper can\'t be updated!"'
+    notify = 'notify-send -u critical "Bing Wallpaper" "Wallpaper can\'t be updated already!"'
     os.system(notify)
